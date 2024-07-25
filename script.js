52ab95b101bbf9a11e2b2b091114fe06dbdfca92
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-07-25 10:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+function capitlize() {
+	const inpVal = document.getElementById("fname");
+	inpVal.value = inpVal.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener('blur',capitlize);
